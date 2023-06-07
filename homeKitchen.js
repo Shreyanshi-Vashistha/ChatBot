01ce06ef5866589a0de158c4e72c034a8fd5ed91
Shreyanshi-Vashistha <Shreyanshi-Vashistha@users.noreply.github.com>
2023-06-06 22:15:50 -0400

    Added Payment method

--- homeKitchen.js
+++ homeKitchen.js
@@ -1,30 +1,40 @@
-const Order = require("./homeKitchenOrder");
+const Order = require("./Order");
 
 const OrderState = Object.freeze({
   WELCOMING: Symbol("welcoming"),
   ITEM: Symbol("item"),
   ITEM2: Symbol("item2"),
+  SIZE: Symbol("size"),
+  SIZE2: Symbol("size2"),
   FILLINGS: Symbol("fillings"),
   FILLINGS2: Symbol("fillings2"),
   DIPS: Symbol("dips"),
   DIPS2: Symbol("dips2"),
+  SIDE: Symbol("side"),
+  SIDE2: Symbol("side2"),
   ORDERAGAIN: Symbol("orderagain"),
+  PAYMENT: Symbol("payment"),
 });
 
-module.exports = class kitchenOrder extends Order {
-  constructor() {
-    super();
+module.exports = class KitchenOrder extends Order {
+  constructor(sNumber, sUrl) {
+    super(sNumber, sUrl);
     this.stateCur = OrderState.WELCOMING;
     this.sItem = "";
     this.sItem2 = "";
+    this.sSize = "";
+    this.sSize2 = "";
     this.sFillings = "";
     this.sFillings2 = "";
     this.sDips = "";
     this.sDips2 = "";
+    this.sSide = "";
+    this.sSide2 = "";
     this.sMenu = "Shreya's Home Kitchen";
 
     // price of first order
     this.cost = 10;
+    this.total = 0;
   }
   handleInput(sInput) {
     let aReturn = [];
@@ -36,15 +46,48 @@ module.exports = class kitchenOrder extends Order {
         aReturn.push("Dumplings or Wraps ?");
         break;
       case OrderState.ITEM:
-        this.stateCur = OrderState.FILLINGS;
-        this.sItem = sInput;
-        aReturn.push("What Fillings would you like?");
-        aReturn.push("Diced Veggies, Diced Meat, Potatoes");
+        this.sItem = sInput.toLowerCase();
+        if (this.sItem != "dumplings" && this.sItem != "wraps") {
+          aReturn.push("Wrong input");
+        } else {
+          this.stateCur = OrderState.SIZE;
+          // this.sItem = sInput;
+          aReturn.push("What size would you like ?");
+          aReturn.push("Large or Small ?");
+        }
+        break;
+      case OrderState.SIZE:
+        this.sSize = sInput.toLowerCase();
+        if (this.sSize != "small" && this.sSize != "large") {
+          aReturn.push("Wrong input");
+        } else {
+          this.stateCur = OrderState.FILLINGS;
+          this.sSize = sInput;
+          aReturn.push("What Fillings would you like?");
+        }
         break;
       case OrderState.FILLINGS:
-        this.stateCur = OrderState.DIPS;
+        this.stateCur = OrderState.SIDE;
         this.sFillings = sInput;
-        aReturn.push("What Dips would you like to have with that?");
+        aReturn.push(
+          "Do you want to have french fries with that in sides, \n Yes or No?"
+        );
+        break;
+      case OrderState.SIDE:
+        this.sSide = sInput.toLowerCase();
+        if (this.sSide != "yes" && this.sSide != "no") {
+          aReturn.push("Wrong input, please enter Yes or No");
+        } else if (this.sSide == "yes") {
+          this.sSide = sInput;
+          this.cost = this.cost + 2;
+        } else {
+          aReturn.push("No French fries");
+        }
+        this.stateCur = OrderState.DIPS;
+        aReturn.push(
+          "What Dips would you like to have with that? \n Type no if you don't want any."
+        );
+
         break;
       case OrderState.DIPS:
         this.stateCur = OrderState.ORDERAGAIN;
@@ -54,45 +97,84 @@ module.exports = class kitchenOrder extends Order {
           this.cost = this.cost + 2;
         }
         aReturn.push(
-          "Would you like to order another item? \n Reply with y/Y or n/N"
+          "Would you like to order another item? \n Reply with Yes or No"
         );
         break;
 
       //cases if user wants to order another item
       case OrderState.ORDERAGAIN:
-        if (sInput.toLowerCase() == "y") {
+        if (sInput.toLowerCase() == "yes") {
           this.stateCur = OrderState.ITEM2;
           aReturn.push("What would you like to order?");
           aReturn.push("Dumplings or Wraps ?");
         }
         // if only one order, display order details and price to the user
-        else if (sInput.toLowerCase() == "n") {
+        else if (sInput.toLowerCase() == "no") {
           aReturn.push("Thank-you for your order from");
           aReturn.push(
-            `${this.sMenu} .Your order is ${this.sItem} with ${this.sFillings} filling`
+            `${this.sMenu} .Your order is ${this.sItem} in ${this.sSize} size with ${this.sFillings} filling`
           );
           if (this.sDips) {
             aReturn.push(`Dip included: ${this.sDips}`);
           }
+          if (this.sSide == "yes") {
+            aReturn.push(` With french fries`);
+          }
           var orderTax = 0.13 * this.cost;
+          this.total = this.cost + orderTax;
           aReturn.push(
             `Your order total is $${this.cost.toFixed(
               2
-            )} \n Total with tax is $${(this.cost + orderTax).toFixed(2)}`
+            )} \n Total with tax is $${this.total.toFixed(2)}`
           );
+          aReturn.push(`Please pay for your order here`);
+          aReturn.push(`${this.sUrl}/payment/${this.sNumber}/`);
+          this.stateCur = OrderState.PAYMENT;
         }
         break;
 
       case OrderState.ITEM2:
-        this.stateCur = OrderState.FILLINGS2;
-        this.sItem2 = sInput;
-        aReturn.push("What Fillings would you like?");
-        aReturn.push("Diced Veggies, Diced Meat, Potatoes");
+        this.sItem2 = sInput.toLowerCase();
+        if (this.sItem2 != "dumplings" && this.sItem2 != "wraps") {
+          aReturn.push("Wrong input");
+        } else {
+          this.stateCur = OrderState.SIZE2;
+          this.sItem2 = sInput;
+          aReturn.push("What size would you like small or large?");
+        }
+        break;
+      case OrderState.SIZE2:
+        this.sSize2 = sInput.toLowerCase();
+        if (this.sSize2 != "small" && this.sSize2 != "large") {
+          aReturn.push("Wrong input");
+        } else {
+          this.stateCur = OrderState.FILLINGS2;
+          this.sSize2 = sInput;
+          aReturn.push("What Fillings would you like?");
+        }
         break;
       case OrderState.FILLINGS2:
-        this.stateCur = OrderState.DIPS2;
+        this.stateCur = OrderState.SIDE2;
         this.sFillings2 = sInput;
-        aReturn.push("What Dips would you like to have with that?");
+        aReturn.push(
+          "Do you want to have french fries with that in sides \n Yes or No?"
+        );
+        break;
+
+      case OrderState.SIDE2:
+        this.sSide2 = sInput.toLowerCase();
+        if (this.sSide2 != "yes" && this.sSide2 != "no") {
+          aReturn.push("Wrong input, please enter Yes or No");
+        } else if (this.sSide2 == "yes") {
+          this.sSide2 = sInput;
+          this.cost = this.cost + 2;
+        } else {
+          aReturn.push("No French fries");
+        }
+        this.stateCur = OrderState.DIPS2;
+        aReturn.push(
+          "What Dips would you like to have with that? \n Type no if you don't want any."
+        );
         break;
       case OrderState.DIPS2:
         this.isDone(true);
@@ -107,31 +189,110 @@ module.exports = class kitchenOrder extends Order {
 
         // display full order details to the user along with price
         aReturn.push(
-          `${this.sMenu} .Your order is ${this.sItem} with ${this.sFillings} filling`
+          `${this.sMenu} .Your order is ${this.sItem} in ${this.sSize} size with ${this.sFillings} filling`
         );
         if (this.sDips) {
           aReturn.push(`Dip included: ${this.sDips}`);
         }
-        if (this.sItem2) {
-          aReturn.push(
-            `Your second order item is ${this.sItem2} with ${this.sFillings2} filling`
-          );
-          if (this.sDips2) {
-            aReturn.push(`Dip included: ${this.sDips2}`);
-          }
+        if (this.sSide == "yes") {
+          aReturn.push(`With French Fries`);
         }
 
-        let d = new Date();
-        d.setMinutes(d.getMinutes() + 20);
+        if (this.sItem2) {
+          aReturn.push(
+            `Your second order item is ${this.sItem2} in ${this.sSize2} size with ${this.sFillings2} filling`
+          );
+        }
+        if (this.sDips2) {
+          aReturn.push(`Dip included: ${this.sDips2}`);
+        }
+        if (this.sSide2 == "yes") {
+          aReturn.push(`With French Fries`);
+        }
+
+        var orderTax = 0.13 * this.cost;
+        this.total = this.cost + orderTax;
         aReturn.push(
           `Your order total is $${this.cost.toFixed(
             2
-          )} \n Total with tax is $${(this.cost + orderTax).toFixed(
-            2
-          )} \nPlease pick up the order at ${d.toTimeString()}`
+          )} \n Total with tax is $${this.total.toFixed(2)}`
         );
+        aReturn.push(`Please pay for your order here`);
+        aReturn.push(`${this.sUrl}/payment/${this.sNumber}/`);
+        this.stateCur = OrderState.PAYMENT;
+
+        break;
+      case OrderState.PAYMENT:
+        this.isDone(true);
+        let d = new Date();
+        d.setMinutes(d.getMinutes() + 20);
+        aReturn.push(` Thanks ${sInput.purchase_units[0].shipping.name["full_name"]} for ordering. \n We'll deliver your order at ${sInput.purchase_units[0].shipping.address["address_line_1"]} 
+        ${sInput.purchase_units[0].shipping.address["admin_area_2"]},
+${sInput.purchase_units[0].shipping.address["admin_area_1"]},
+        ${sInput.purchase_units[0].shipping.address["postal_code"]},
+        ${sInput.purchase_units[0].shipping.address["country_code"]}
+        `);
+        aReturn.push(`Your order will be delivered at ${d.toTimeString()}`);
         break;
     }
     return aReturn;
+  }
+
+  renderForm(sTitle = "-1", sAmount = "-1") {
+    // your client id should be kept private
+    if (sTitle != "-1") {
+      this.sItem = sTitle;
+    }
+    if (sAmount != "-1") {
+      this.nOrder = sAmount;
+    }
+    const sClientID = process.env.SB_CLIENT_ID || console.log(sClientID);
+    ("put your client id here for testing ... Make sure that you delete it before committing");
+    return `
+      <!DOCTYPE html>
+  
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+      </head>
+      
+      <body>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script
+          src="https://www.paypal.com/sdk/js?client-id=${sClientID}"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+        </script>
+        Thank you ${this.sNumber} for your order of $${this.total}.
+        <div id="paypal-button-container"></div>
+  
+        <script>
+          paypal.Buttons({
+              createOrder: function(data, actions) {
+                // This function sets up the details of the transaction, including the amount and line item details.
+                return actions.order.create({
+                  purchase_units: [{
+                    amount: {
+                      value: '${this.total}'
+                    }
+                  }]
+                });
+              },
+              onApprove: function(data, actions) {
+                // This function captures the funds from the transaction.
+                return actions.order.capture().then(function(details) {
+                  // This function shows a transaction success message to your buyer.
+                  $.post(".", details, ()=>{
+                    window.open("", "_self");
+                    window.close(); 
+                  });
+                });
+              }
+          
+            }).render('#paypal-button-container');
+          // This function displays Smart Payment Buttons on your web page.
+        </script>
+      
+      </body>
+          
+      `;
   }
 };
--- index.js
+++ index.js
@@ -1,28 +1,82 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const kitchenOrder = require("./homeKitchen");
+const app = express();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
+const _ = require("underscore");
+
+const port = process.env.PORT || parseInt(process.argv.pop()) || 3002;
+
+server.listen(port, function () {
+  console.log("Server listening at port %d", port);
+});
+
+const KitchenOrder = require("./homeKitchen");
+const e = require("express");
+const { exception } = require("console");
 
 // Create a new express application instance
-const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("www"));
 
-app.get("/users/:uname", (req, res) => {
-  res.end("Hello " + req.params.uname);
+let oSockets = {};
+let oOrders = {};
+app.post("/payment/:phone", (req, res) => {
+  // this happens when the order is complete
+  sFrom = req.params.phone;
+  const aReply = oOrders[sFrom].handleInput(req.body);
+  const oSocket = oSockets[sFrom];
+  // send messages out of turn
+  for (let n = 0; n < aReply.length; n++) {
+    if (oSocket) {
+      const data = {
+        message: aReply[n],
+      };
+      oSocket.emit("receive message", data);
+    } else {
+      throw new Exception("twilio code would go here");
+    }
+  }
+  if (oOrders[sFrom].isDone()) {
+    delete oOrders[sFrom];
+    delete oSockets[sFrom];
+  }
+  res.end("ok");
 });
 
-let oOrders = {};
-app.post("/sms", (req, res) => {
-  let sFrom = req.body.From || req.body.from;
+app.get("/payment/:phone", (req, res) => {
+  // this happens when the user clicks on the link in SMS
+  const sFrom = req.params.phone;
   if (!oOrders.hasOwnProperty(sFrom)) {
-    oOrders[sFrom] = new kitchenOrder();
+    res.end("order already complete");
+  } else {
+    res.end(oOrders[sFrom].renderForm());
   }
-  let sMessage = req.body.Body || req.body.body;
-  let aReply = oOrders[sFrom].handleInput(sMessage);
+});
+
+app.post("/payment", (req, res) => {
+  // this happens when the user clicks on the link in SMS
+  //const sFrom = req.params.phone;
+  const sFrom = req.body.telephone;
+  oOrders[sFrom] = new KitchenOrder(sFrom);
+  res.end(oOrders[sFrom].renderForm(req.body.title, req.body.price));
+});
+
+app.post("/sms", (req, res) => {
+  // turn taking SMS
+  let sFrom = req.body.From || req.body.from;
+  let sUrl = `${req.headers["x-forwarded-proto"] || req.protocol}://${
+    req.headers["x-forwarded-host"] || req.headers.host
+  }${req.baseUrl}`;
+  if (!oOrders.hasOwnProperty(sFrom)) {
+    oOrders[sFrom] = new KitchenOrder(sFrom, sUrl);
+  }
   if (oOrders[sFrom].isDone()) {
     delete oOrders[sFrom];
   }
+  let sMessage = req.body.Body || req.body.body;
+  let aReply = oOrders[sFrom].handleInput(sMessage);
   res.setHeader("content-type", "text/xml");
   let sResponse = "<Response>";
   for (let n = 0; n < aReply.length; n++) {
@@ -33,8 +87,11 @@ app.post("/sms", (req, res) => {
   res.end(sResponse + "</Response>");
 });
 
-var port = process.env.PORT || parseInt(process.argv.pop()) || 3002;
-
-app.listen(port, () =>
-  console.log("Example app listening on port " + port + "!")
-);
+io.on("connection", function (socket) {
+  // when the client emits 'receive message', this listens and executes
+  socket.on("receive message", function (data) {
+    // set up a socket to send messages to out of turn
+    const sFrom = _.escape(data.from);
+    oSockets[sFrom] = socket;
+  });
+});
